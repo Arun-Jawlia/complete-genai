edba8221c4c6d8a254393561ca7c408519a528b4
Arun-Jawlia <arunkumar08.mk@gmail.com>
2026-05-31 22:00:03 +0530

    feat: task 1 of numpy

---
+++ 5_Numpy/Assignments/GenAI-Assignments-9-Arun-Jawlia/task_1.py
@@ -0,0 +1,28 @@
+# Task 1: Creating NumPy Arrays
+
+import numpy as np
+arr1 = np.arange(1, 11)
+
+arr2 = np.arange(1, 10).reshape(3, 3)
+
+arr3 = np.array([10, 20, 30, 40, 50])
+
+print(arr1)
+
+print("\n2D Array:")
+print(arr2)
+
+print("\nArray from List:")
+print(arr3)
+
+# Print shape of each array
+print("\nShapes:")
+print("arr1 shape:", arr1.shape)
+print("arr2 shape:", arr2.shape)
+print("arr3 shape:", arr3.shape)
+
+# Print data type of each array
+print("\nData Types:")
+print("arr1 dtype:", arr1.dtype)
+print("arr2 dtype:", arr2.dtype)
+print("arr3 dtype:", arr3.dtype)
